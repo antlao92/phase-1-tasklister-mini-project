94ebc51f1d85a6d9ed52303a100f142016fd2497
Anthony <anthonykimlao@gmail.com>
2024-01-08 19:26:36 -0600

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e)=>{
+    e.preventDefault()
+    buildToDo(e.target.new_task_description.value)
+  })
 });
+
+
+function buildToDo(toDo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  let priority = document.createElement('select',)
+  btn.addEventListener('click', handleDel)
+  btn.textContent = 'x '
+  p.textContent = `${toDo} ` 
+  p.appendChild(btn)
+  p.appendChild(priority)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDel(e){
+  e.target.parentNode.remove()
+}
